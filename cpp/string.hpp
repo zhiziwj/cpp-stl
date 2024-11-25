e36c85d0e20f75bd6c73b2a08681c26a29f4dcb5
zhiziwj <zhizi6@tutamail.com>
2024-11-25 18:02:14 +0800

    Add files via upload

---
+++ cpp/string.hpp
@@ -0,0 +1,53 @@
+#ifndef MSTRING_H
+#define MSTRING_H
+#include <cstring>
+class mstring{
+	private:
+	char* str;
+	long long int len;//the string long
+	public:
+	mstring(){
+		str=nullptr;
+		len=0;
+	}
+	mstring(const char *s){
+		len=strlen(str);
+		str=new char[len+1];//give data space
+	}
+	void clear(){
+		delete[] str;
+		len=0;
+	}
+	long long int size()const{
+		return len;
+	}
+	const char* c_str()const{
+		return str;
+	}
+	mstring(const mstring& s){//copy
+		len=s.size();
+		str=new char(len+1);
+		strcpy(str,s.str);
+	}
+	mstring operator =(const mstring& s){
+		if(this!=&s){//the s is not me
+			delete[] str;//delete the space
+			len=s.len;
+			str=new char[len+1];
+			strcpy(str,s.str);
+		}
+		return *this;
+	}
+	mstring operator +(const mstring& s){
+		mstring temp;
+		temp.clear();
+		temp.len=len+s.len;
+		strcpy(temp.str,str);
+		strcat(temp.str,s.str);
+		return temp;
+	}
+	bool operator==(const mstring& s){
+		return (strcmp(str,s.str)==0);
+	}
+};
+#endif //MSTRING_H
